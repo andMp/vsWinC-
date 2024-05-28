984941452064b00d5b66c0fa2dee05b0db502461
andMp <myposk@gmail.com>
2024-05-28 14:03:21 +0300

    Add project files.

---
+++ Project/main.cpp
@@ -0,0 +1,19 @@
+﻿#include"String.h"
+/*Екзамен.
+Створити програму для шифрування та дешифрування тексту за допомогою шифру Цезаря та шифру XOR. 
+Використати для цього наслідування та абстрактні класи.*/
+int main()
+{   
+	String a;
+	String b;
+	a.nabirT();
+	b.nabirT();
+	a.Print();
+	b.Print();
+	cout << a[3] << '\n';
+	cout << b('b') << '\n';
+	cout << "Operacia prefiksna '--':\n";
+	a = --b;
+	a.Print();
+	b.Print();
+}
